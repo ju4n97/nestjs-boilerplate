354543392b90646ac35f9ed6474f48880e138a83
juanmesa2097 <juanmesa2097@gmail.com>
2020-09-05 16:56:44 -0500

    Create connection provider for mysql

---
+++ src/providers/database/mysql/mysql-provider.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { MysqlConfigModule } from 'src/config/database/mysql/mysql-config.module';
+import { MysqlConfigService } from 'src/config/database/mysql/mysql-config.service';
+import { DatabaseType } from 'typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [MysqlConfigModule],
+      useFactory: async (mysqlConfigService: MysqlConfigService) => ({
+        type: 'mysql' as DatabaseType,
+        host: mysqlConfigService.host,
+        port: mysqlConfigService.port,
+        username: mysqlConfigService.username,
+        password: mysqlConfigService.password,
+        database: mysqlConfigService.database,
+        underscore: true,
+        entities: [__dirname + '/../../../**/*.entity.{js,ts}'],
+        synchronize: mysqlConfigService.synchronize,
+      }),
+      inject: [MysqlConfigService],
+    } as TypeOrmModuleAsyncOptions),
+  ],
+})
+export class MysqlProviderModule {}
