6d5fe0dd1409d6430667d38a723a93e300739a78
juanmesa2097 <juanmesa2097@gmail.com>
2020-09-05 18:06:55 -0500

    Create users module

---
+++ src/modules/users/users.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserRepository])],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
+export class UsersModule {}
