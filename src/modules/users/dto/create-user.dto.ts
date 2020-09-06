f36bc6ea8dce0fa4f15fa00331bf8a34e7fced19
juanmesa2097 <juanmesa2097@gmail.com>
2020-09-05 23:43:43 -0500

    Update "create user" dto

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,8 +1,8 @@
-import { PickType } from '@nestjs/mapped-types';
+import { IntersectionType, PickType } from '@nestjs/mapped-types';
+import { UserDetailDto } from './user-detail.dto';
 import { UserDto } from './user.dto';
 
-export class CreateUserDto extends PickType(UserDto, [
-  'username',
-  'password',
-  'email',
-] as const) {}
+export class CreateUserDto extends IntersectionType(
+  PickType(UserDto, ['username', 'password', 'email'] as const),
+  PickType(UserDetailDto, ['firstName', 'lastName'] as const),
+) {}
