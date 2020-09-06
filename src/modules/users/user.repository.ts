97f86597d763a657a31acdaac8baf882dc5bedfb
juanmesa2097 <juanmesa2097@gmail.com>
2020-09-05 23:38:26 -0500

    Move user entities to directory

--- src/modules/users/user.repository.ts
+++ src/modules/users/user.repository.ts
@@ -1,5 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm';
-import { UserEntity } from './user.entity';
+import { UserEntity } from './entities/user.entity';
 
 @EntityRepository(UserEntity)
 export class UserRepository extends Repository<UserEntity> {}
