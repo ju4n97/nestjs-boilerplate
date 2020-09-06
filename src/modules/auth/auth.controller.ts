cbc954cb49d9c9f680090298690a26b50d9883c8
juanmesa2097 <juanmesa2097@gmail.com>
2020-09-06 01:00:35 -0500

    Implement auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto, GetUserDto, LoginUserDto } from '../users/dto';
+import { AuthService } from './auth.service';
+import { LoginResult } from './interfaces';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly _authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto): Promise<GetUserDto> {
+    return await this._authService.register(createUserDto);
+  }
+
+  @Post('login')
+  async login(@Body() loginUserDto: LoginUserDto): Promise<LoginResult> {
+    return await this._authService.login(loginUserDto);
+  }
+}
