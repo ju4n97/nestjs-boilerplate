84c22fd0498c301fdcc56c64e2ab9d046ae252d0
juanmesa2097 <juanmesa2097@gmail.com>
2020-09-06 01:01:03 -0500

    Configure Passport module and Jwt module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,33 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Logger, Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthConfigModule } from 'src/config/auth/auth-config.module';
+import { AuthConfigService } from 'src/config/auth/auth-config.service';
+import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [
+    UsersModule,
+    AuthConfigModule,
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+      property: 'user',
+      session: false,
+    }),
+    JwtModule.registerAsync({
+      imports: [AuthConfigModule],
+      useFactory: (authConfigService: AuthConfigService) => ({
+        secret: authConfigService.jwtSecretKey,
+        signOptions: {
+          expiresIn: authConfigService.jwtExpiresIn,
+        },
+      }),
+      inject: [AuthConfigService],
+    }),
+  ],
+  providers: [AuthService, AuthConfigService, Logger],
+  controllers: [AuthController],
 })
 export class AuthModule {}
