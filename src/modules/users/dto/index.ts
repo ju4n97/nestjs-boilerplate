3bab73383a65c02534917d0d7e34a508213cc9d2
juanmesa2097 <juanmesa2097@gmail.com>
2020-09-06 01:04:28 -0500

    Update user's barrel technique

--- src/modules/users/dto/index.ts
+++ src/modules/users/dto/index.ts
@@ -1,2 +1,4 @@
 export { CreateUserDto } from './create-user.dto';
+export { GetUserDetailDto } from './get-user-detail.dto';
 export { GetUserDto } from './get-user.dto';
+export { LoginUserDto } from './login-user.dto';
