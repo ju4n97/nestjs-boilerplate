ebd3865be4caeb163eb96704ea05ae294f9ee611
juanmesa2097 <juanmesa2097@gmail.com>
2020-09-15 18:38:06 -0500

    Configure email provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,16 @@ import { AppConfigService } from './config/app/app-config.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { MysqlProviderModule } from './providers/database/mysql/mysql-provider.module';
+import { EmailProviderModule } from './providers/email/email-provider.module';
 
 @Module({
-  imports: [AppConfigModule, MysqlProviderModule, AuthModule, UsersModule],
+  imports: [
+    AppConfigModule,
+    MysqlProviderModule,
+    EmailProviderModule,
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService, AppConfigService],
 })
