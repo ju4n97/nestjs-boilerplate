38ae46917d59158604b553ebcc7f2ab076373379
juanmesa2097 <juanmesa2097@gmail.com>
2020-10-09 23:43:16 -0500

    Create utility to perform dynamic query filters

--- src/common/utils/advance-result/advance-result.util.ts
+++ src/common/utils/advance-result/advance-result.util.ts
@@ -1,3 +1,16 @@
+import { FilterOperator } from '@lib/enums/advance-result';
+import {
+  Between,
+  Equal,
+  FindOperator,
+  In,
+  LessThan,
+  LessThanOrEqual,
+  Like,
+  MoreThan,
+  Not,
+} from 'typeorm';
+
 /**
  * @description Defines the structure of a selection object.
  * @param selectParam Query parameter of selection (select).
@@ -27,4 +40,105 @@ export const mapRelations = (relationsParams: string): any => {
   }
 
   return relationsParams.split(',').map(r => r);
+};
+
+/**
+ * @description Defines the structure of a filter object.
+ * @param filterParam Query parameter of filter (filter).
+ * @returns A dynamic array of objects that will be processed by typeorm, which allows dynamic queries.
+ */
+export const mapFilter = (filterParam: string): Record<string, string>[] => {
+  if (!filterParam) {
+    return null;
+  }
+
+  // Matches first condition.
+  const first = (filterParam.match(/\[\[.+?\]/) || []).map(x => x.slice(1));
+
+  // Determine if the first condition is an OR or an AND.
+  const firstOr =
+    filterParam.indexOf('or') > -1 ? filterParam.indexOf('or') : 9999999;
+  const firstAnd =
+    filterParam.indexOf('and') > -1 ? filterParam.indexOf('and') : 9999999;
+  const firstConditionOr = firstOr < firstAnd;
+
+  // Matches OR and AND conditions inside the query parameter.
+  let ands = (filterParam.match(/"and",\[.+?\]/gi) || []).map(x => x.slice(6));
+  let ors = (filterParam.match(/"or",\[.+?\]/gi) || []).map(x => x.slice(5));
+
+  // Concatenates the first condition to the conditional arrays based on their state.
+  if (firstConditionOr) {
+    ors = [...first, ...ors];
+  } else {
+    ands = [...first, ...ands];
+  }
+
+  let andConditions = {};
+  const finalCondition = [];
+
+  // Loops through the AND conditions and define a structure to be merged with the OR conditionals.
+  for (const and of ands) {
+    andConditions = Object.assign(andConditions, {
+      ..._destructureArray(JSON.parse(and)),
+    });
+
+    // If there are no OR conditionals, it sets the AND conditionals as final.
+    if (ors.length === 0) {
+      finalCondition.push(andConditions);
+    }
+  }
+
+  // Generates separated objects to perform the OR queries and concatenates the AND conditionals to them.
+  for (const or of ors) {
+    finalCondition.push({
+      ...andConditions,
+      ..._destructureArray(JSON.parse(or)),
+    });
+  }
+
+  return finalCondition;
+};
+
+/**
+ * @description Gets the values of a 3-position array and returns a new object
+ * @param array 3-position Array ([key, filterOperator, value]).
+ * @returns An object that will be processed by typeorm.
+ */
+const _destructureArray = (array: Array<string>) => {
+  const key = array[0];
+  const filterOperator = array[1] as FilterOperator;
+  const value = array[2];
+
+  return {
+    [key]: _getfilterExpression(value, filterOperator),
+  };
+};
+
+/**
+ * @description Defines the operator to use to execute a filter expression.
+ * @param value Filter value.
+ * @param filterOperator Filter operator
+ * @returns Filter value inside typeorm FindOperator expresion.
+ */
+const _getfilterExpression = (
+  value: string,
+  filterOperator: FilterOperator,
+): FindOperator<string> => {
+  return {
+    [FilterOperator.Equals]: Equal(value),
+    [FilterOperator.NotEquals]: Not(Equal(value)),
+    [FilterOperator.Contains]: Like(`%${value}%`),
+    [FilterOperator.NotContain]: Not(Like(`%${value}%`)),
+    [FilterOperator.StartsWith]: Like(`${value}%`),
+    [FilterOperator.NotStartWith]: Not(Like(`${value}%`)),
+    [FilterOperator.EndsWith]: Like(`%${value}`),
+    [FilterOperator.NotEndWith]: Not(Like(`%${value}`)),
+    [FilterOperator.MoreThan]: MoreThan(value),
+    [FilterOperator.MoreThanOrEqual]: LessThanOrEqual(value),
+    [FilterOperator.LessThan]: LessThan(value),
+    [FilterOperator.LessThanOrEqual]: LessThanOrEqual(value),
+    [FilterOperator.In]: In(value.split('|')),
+    [FilterOperator.NotIn]: Not(In(value.split('|'))),
+    [FilterOperator.Between]: Between(value.split('|')[0], value.split('|')[1]),
+  }[filterOperator];
 };
