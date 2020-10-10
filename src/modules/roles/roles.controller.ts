0d1f20f7938804e793854a46ded418fe24477593
juanmesa2097 <juanmesa2097@gmail.com>
2020-10-10 00:03:12 -0500

    Implement role repository

--- src/modules/roles/roles.controller.ts
+++ src/modules/roles/roles.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
+import { RoleRepository } from './role.repository';
 
 @Controller('roles')
-export class RolesController {}
+export class RolesController {
+  constructor(
+    private readonly _roleRepository: RoleRepository,
+    private readonly _logger: Logger,
+  ) {
+    this._logger.setContext('RolesService');
+  }
+}
--- src/modules/roles/roles.module.ts
+++ src/modules/roles/roles.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoleRepository } from './role.repository';
 import { RolesController } from './roles.controller';
 import { RolesService } from './roles.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RoleRepository])],
   controllers: [RolesController],
-  providers: [RolesService]
+  providers: [RolesService, Logger],
 })
 export class RolesModule {}
