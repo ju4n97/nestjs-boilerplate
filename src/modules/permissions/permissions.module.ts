4c6d4505ec9d250404758977ecc35d80acb807c4
juanmesa2097 <juanmesa2097@gmail.com>
2020-10-11 18:11:44 -0500

    Protect permissions controller

--- src/modules/permissions/permissions.module.ts
+++ src/modules/permissions/permissions.module.ts
@@ -1,11 +1,12 @@
 import { Logger, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
 import { PermissionRepository } from './permission.repository';
 import { PermissionsController } from './permissions.controller';
 import { PermissionsService } from './permissions.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PermissionRepository])],
+  imports: [TypeOrmModule.forFeature([PermissionRepository]), AuthModule],
   providers: [PermissionsService, Logger],
   controllers: [PermissionsController],
 })
