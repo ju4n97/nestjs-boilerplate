a447e01a4e41fc28cf50b106187f1f50f7a7caf2
juanmesa2097 <juanmesa2097@gmail.com>
2020-10-11 23:13:11 -0500

    Create validation messages for file filters

---
+++ src/common/enums/messages/validation-message.enum.ts
@@ -0,0 +1,15 @@
+export enum ValidationMessage {
+  OnlyImagesFilesAllowed = 'Only "(JPG | JPEG | PNG | GIF)" files are allowed.',
+  OnlyJpgFilesAllowed = 'Only "(JPG | JPEG)" files are allowed.',
+  OnlyPngFilesAllowed = 'Only "PNG" files are allowed.',
+  OnlyGifFilesAllowed = 'Only "GIF" files are allowed.',
+  OnlyCsvFilesAllowed = 'Only "CSV" files are allowed.',
+  OnlyTxtFilesAllowed = 'Only "TXT" files are allowed.',
+  OnlyPdfFilesAllowed = 'Only "PDF" files are allowed.',
+  OnlyDocxFilesAllowed = 'Only "DOCX" files are allowed.',
+  OnlyXlsxFilesAllowed = 'Only "XLSX" files are allowed.',
+  OnlyXlsmFilesAllowed = 'Only "XLSM" files are allowed.',
+  OnlyPptxFilesAllowed = 'Only "PPTX" files are allowed.',
+  OnlyJsonFilesAllowed = 'Only "JSON" files are allowed.',
+  OnlyXmlFilesAllowed = 'Only "XML" files are allowed.',
+}
