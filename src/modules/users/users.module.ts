d3710f812d60fcf752f2fc4e66f0044e43757a05
juanmesa2097 <juanmesa2097@gmail.com>
2020-10-11 23:20:24 -0500

    Register multer and files modules

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,6 +1,8 @@
 import { forwardRef, Logger, Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express/multer/multer.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '../auth/auth.module';
+import { FilesModule } from '../files/files.module';
 import { UserRepository } from './user.repository';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -9,6 +11,8 @@ import { UsersService } from './users.service';
   imports: [
     TypeOrmModule.forFeature([UserRepository]),
     forwardRef(() => AuthModule),
+    MulterModule.register(),
+    FilesModule,
   ],
   controllers: [UsersController],
   providers: [UsersService, Logger],
