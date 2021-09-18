bee0febd7c775605d759c9f858c5a3cbbba1b696
juanmesa2097 <juanmesa2097@gmail.com>
2021-09-17 20:43:33 -0500

    Create config & graphql providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
-import config from 'src/config/config';
-import { GraphqlConfig } from './config/config.interface';
+import { ConfigProviderModule } from './providers/config-provider.module';
+import { GraphqlProviderModule } from './providers/graphql-provider.module';
 import { UsersModule } from './user/users.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [config],
-    }),
-    GraphQLModule.forRootAsync({
-      useFactory: async (configService: ConfigService) => {
-        const config = configService.get<GraphqlConfig>('graphql');
-        return {
-          autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-          sortSchema: config.sortSchema,
-          debug: config.debug,
-          playground: config.playground,
-          introspection: config.introspection,
-        };
-      },
-      inject: [ConfigService],
-    }),
-    UsersModule,
-  ],
-  controllers: [],
-  providers: [],
+  imports: [ConfigProviderModule, GraphqlProviderModule, UsersModule],
 })
 export class AppModule {}
---
+++ src/providers/graphql-provider.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { GraphqlConfig } from 'src/config/config.interface';
+
+@Module({
+  imports: [
+    GraphQLModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => {
+        const config = configService.get<GraphqlConfig>('graphql');
+        return {
+          autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+          sortSchema: config.sortSchema,
+          debug: config.debug,
+          playground: config.playground,
+          introspection: config.introspection,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class GraphqlProviderModule {}
